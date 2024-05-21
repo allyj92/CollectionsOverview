96812dbd09ddbddcb1bb733a449d5110016d17a2
eunjaelim <imeunjae361@gmail.com>
2024-05-21 16:09:47 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Collection<String> list = new HashSet<>();
+
+        String[] names = {"Anna", "Bob","Carol","David","Edna"};
+        list.addAll(Arrays.asList(names));
+        System.out.println(list);
+
+        list.add("Fred");
+
+        System.out.println(list);
+
+        list.addAll(Arrays.asList("George","Gary","Grace"));
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+
+        list.removeIf(s -> s.charAt(0) == 'G');
+        System.out.println(list);
+        System.out.println("Gary is in the list? " + list.contains("Gary"));
+//        list.sort();
+    }
+}
